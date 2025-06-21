72588e8ec7287c1216ba86fc55a3bd15573b7bf4
mjr2595 <michaeljohnraymond@gmail.com>
2025-06-21 00:52:25 -0500

    Refactor main function to improve argument handling and add verbose output option

--- main.py
+++ main.py
@@ -3,30 +3,45 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-if len(sys.argv) != 2:
-    print("Usage: python main.py <prompt>")
-    sys.exit(1)
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
 
-if not api_key:
-    print("Error: GEMINI_API_KEY environment variable is not set.")
-    sys.exit(1)
+    args = sys.argv[1:]
+    verbose = False
+    if "--verbose" in args:
+        verbose = True
+        args.remove("--verbose")
 
-client = genai.Client(api_key=api_key)
-model = "gemini-2.0-flash-001"
-prompt = sys.argv[1]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build an AI startup?" --verbose')
+        sys.exit(1)
+    user_prompt = " ".join(args)
 
-response = client.models.generate_content(model=model, contents=prompt)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    model = "gemini-2.0-flash-001"
 
-print("\nModel's response:")
-print(response.text)
+    user_prompt = " ".join(args)
 
-print("\nToken usage:")
-if response.usage_metadata is not None:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-else:
-    print("Token usage metadata is not available.")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(model=model, contents=messages)
+    print(response.text)
+
+    if verbose:
+        print("\n**Verbose output**")
+        print(f"User prompt: {user_prompt}")
+        if response.usage_metadata is not None:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
