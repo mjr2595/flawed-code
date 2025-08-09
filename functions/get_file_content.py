4b22e443bfe555c609e0c088641c2a763567ee00
mjr2595 <michaeljohnraymond@gmail.com>
2025-08-08 23:53:13 -0500

    added more functions

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,9 @@
 import os
 
+from google.genai import types
+
+from config import MAX_CHARS
+
 
 def get_file_content(working_directory, file_path):
     try:
@@ -24,3 +28,19 @@ def get_file_content(working_directory, file_path):
         return content
     except Exception as e:
         return f"Error: {str(e)}"
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
